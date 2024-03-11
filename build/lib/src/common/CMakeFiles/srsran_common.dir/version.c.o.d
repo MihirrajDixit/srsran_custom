9c7bbdc9be9ed4e856caab3ea020f5611194cb66
mihirraj dixit <dixitmn6@gmail.com>
2024-03-11 15:17:18 +0100

    removed build

--- build/lib/src/common/CMakeFiles/srsran_common.dir/version.c.o.d
+++
@@ -1,5 +0,0 @@
-lib/src/common/CMakeFiles/srsran_common.dir/version.c.o: \
- /home/anonymous/srsRAN_4G/lib/src/common/version.c \
- /usr/include/stdc-predef.h \
- /home/anonymous/srsRAN_4G/build/lib/include/srsran/version.h \
- /home/anonymous/srsRAN_4G/lib/include/srsran/config.h
--- build/lib/src/common/CMakeFiles/srsran_common.dir/zuc.cc.o.d
+++
@@ -1,4 +0,0 @@
-lib/src/common/CMakeFiles/srsran_common.dir/zuc.cc.o: \
- /home/anonymous/srsRAN_4G/lib/src/common/zuc.cc \
- /usr/include/stdc-predef.h \
- /home/anonymous/srsRAN_4G/lib/include/srsran/common/zuc.h
